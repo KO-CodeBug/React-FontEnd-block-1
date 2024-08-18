173eedcf19ed5c1e2f12ca1d5f9bb280db2ec329
KO-CodeBug <ko.2505004@gmail.com>
2024-08-18 23:53:16 +0700

    setting pages user

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -3,26 +3,37 @@ import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 
 import {fetchAllUser} from '../services/UserServices'
+import ReactPaginate from 'react-paginate';
+
 
 const TableUsers = (props) => {
     const [listUser, setListUsers] = useState([]);
+    const [totalUser , setTotalUsers] = useState(0);
+    const [totalPages , setTotalPages] = useState(0);
+
 
     useEffect(()=>{
         // call API // dry (vô hạn code quá 2 lần code tối ưu)
-        getUser();
+        getUser(1);
     },[])
 
-    const getUser = async () => {
-        let res = await fetchAllUser();
+    const getUser = async (page) => {
+        let res = await fetchAllUser(page);
         console.log('cheack new res' , res);
         
         if(res && res.data ) {
+            console.log(res.data);
+            setTotalUsers(res.total)
+            setTotalPages(res.total_pages)
             setListUsers(res.data)
         }
     }
     console.log(listUser);
     
-
+    const handlePageClick = (event) => {
+        getUser(+ event.selected + 1);
+        // console.log('cheack event ', event);        
+    }
     return(
         <>
             <Table striped bordered hover>
@@ -51,6 +62,25 @@ const TableUsers = (props) => {
                     
                 </tbody>
             </Table>
+
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={totalPages}
+                previousLabel="< previous"
+                pageClassName="page-item"
+                pageLinkClassName="page-link"
+                previousClassName="page-item"
+                previousLinkClassName="page-link"
+                nextClassName="page-item"
+                nextLinkClassName="page-link"
+                breakClassName="page-item"
+                breakLinkClassName="page-link"
+                containerClassName="pagination"
+                activeClassName="active"
+            />
         </>
     )
 }
